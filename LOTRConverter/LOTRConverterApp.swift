773573f94cc12fcebf20fde253fbf0ec9042285b
Kenneth Jones <kennethjones1991@outlook.com>
2023-01-25 14:13:10 -0700

    Initial project commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Kenneth Jones on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
