ef79b9b260108cd262e550ede68e6f2999d2994d
Kenneth Jones <kennethjones1991@outlook.com>
2023-01-25 15:14:27 -0700

    End of lesson 6; create new SwiftUI view for exchange rates and add them to ExchangeInfo view

---
+++ LOTRConverter/ExchangeRate.swift
@@ -0,0 +1,40 @@
+//
+//  ExchangeRate.swift
+//  LOTRConverter
+//
+//  Created by Kenneth Jones on 1/25/23.
+//
+
+import SwiftUI
+
+struct ExchangeRate: View {
+    @State var leftImage: String
+    @State var text: String
+    @State var rightImage: String
+    
+    var body: some View {
+        HStack {
+            // Left currency image
+            Image(leftImage)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 33)
+            
+            // Exchange rate text
+            Text(text)
+            
+            // Right currency image
+            Image(rightImage)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 33)
+        }
+    }
+}
+
+struct ExchangeRate_Previews: PreviewProvider {
+    static var previews: some View {
+        ExchangeRate(leftImage: "goldpenny", text: "1 Gold Penny = 4 Silver Pieces", rightImage: "silverpiece")
+            .previewLayout(.sizeThatFits)
+    }
+}
